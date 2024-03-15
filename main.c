de53d2b2829bd392ec296ff36fc0f7e1b6a0de7c
tgcl21 <tgcl21@inf.ufpr.br>
2024-03-15 17:19:33 -0300

    scan variables

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, int *argv[]) {
+    int cidades, rotas, recursos, pacotes, ganho;
+    
+    scanf("%d %d %d %d %d", &cidades, &rotas, &recursos, &pacotes, &ganho);
+        
+    return 0;
+}
