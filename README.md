658c883c3e69bb9fc258934ed54faf57bffca223
MTlls <matheustellestab8@gmail.com>
2024-04-09 05:46:11 -0300

    [updated] readme, main

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # t1-otimizacao
+
+O trabalho atual foi realizado com o intuito de resolver o problema apresentado no primeiro trabalho na disciplina de Otimização.
--- main.cpp
+++ main.cpp
@@ -13,14 +13,9 @@ int main(){
     vector<vector<int>> vetor_pacotes;
     vetor_pacotes = le_matriz(qtd_pacotes, qtd_recursos + 1);
 
-    // printData(qtd_cidades, qtd_rotas, qtd_recursos, qtd_pacotes, ganho, vetor_pacotes, matriz_de_rotas);
-    
     funcao_objetivo(ganho, vetor_pacotes, qtd_pacotes);
     rotas(matriz_de_rotas);
     recebido(qtd_cidades, matriz_de_rotas);
-
-    // recursos_pacotes(qtd_pacotes, vetor_pacotes, qtd_recursos);
-    // recursos_rotas(qtd_rotas, matriz_de_rotas, qtd_recursos);
 
     modulo_rota(matriz_de_rotas);
     recurso_necessario(matriz_de_rotas, qtd_recursos);
